ec53406c99c1303d676b7d36791aa0aab56b3a31
pcsang <pcsangb1706521@gmail.com>
2022-03-07 18:09:58 +0700

    bookServiceTest.java

--- src/test/java/com/example/demobook/books/BookControllerTest.java
+++ src/test/java/com/example/demobook/books/BookControllerTest.java
@@ -2,9 +2,11 @@ package com.example.demobook.books;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -16,14 +18,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
     @MockBean
     private BookService bookService;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+//    @Autowired
+//    private WebApplicationContext webApplicationContext;
 
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/example/demobook/books/BookServiceTest.java
+++ src/test/java/com/example/demobook/books/BookServiceTest.java
@@ -1,12 +1,18 @@
 package com.example.demobook.books;
 
+import org.apache.el.parser.AstSetData;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -16,6 +22,7 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,12 +31,15 @@ class BookServiceTest {
 
     @Mock
     private BookRepository bookRepository;
+
+    @InjectMocks
     private BookService bookUnderService;
 
     @BeforeEach
     void setUp() {
         bookUnderService = new BookService(bookRepository);
     }
+
 
     @Test
     void getBook() {
@@ -47,8 +57,8 @@ class BookServiceTest {
         bookUnderService.addNewBook(book);
         ArgumentCaptor<Book> bookArgumentCaptor = ArgumentCaptor.forClass(Book.class);
         verify(bookRepository).save(bookArgumentCaptor.capture());
-//        Book captrueBook = bookArgumentCaptor.getValue();
-//        assertThat(captrueBook).isEqualTo(book);
+        Book captrueBook = bookArgumentCaptor.getValue();
+        assertThat(captrueBook).isEqualTo(book);
     }
 
     @Test
@@ -59,13 +69,16 @@ class BookServiceTest {
                 "Khoahoc", "quan su",
                 LocalDate.of(2010,05,12),
                 LocalDate.of(2015,05,15));
-        bookUnderService.addNewBook(book);
+        Book book2 =  new Book(2, "The war", "phamsang",
+                "Khoahoc", "quan su",
+                LocalDate.of(2010,05,12),
+                LocalDate.of(2015,05,15));
+        bookRepository.saveAll(List.of(book, book2));
 
-        //given(bookRepository.existsById(1)).willReturn(true);
         assertThatThrownBy(()->bookUnderService.deleteBook(1))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("Book with id "+ 1 + " does not exists");
-        verify(bookRepository).existsById(1);
+
     }
 
     @Test
@@ -81,17 +94,7 @@ class BookServiceTest {
                 LocalDate.of(2010,05,12),
                 LocalDate.of(2015,05,15));
         bookUnderService.addNewBook(book2);
-//        List<Book> books = List.of(book,book2);
-//
-//        //given(bookRepository.existsById(1)).willReturn(true);
-//        assertThatThrownBy(()->bookUnderService.deleteBook(1))
-//                .isInstanceOf(IllegalStateException.class)
-//                .hasMessageContaining("Book with id "+ 1 + " does not exists");
-//
-       List<Book> bookAfterDelete = bookUnderService.getBook();
-//
-//        assertThat(books).isNotEqualTo(bookAfterDelete);
-        System.out.println(bookAfterDelete.size());
+        List<Book> books = List.of(book);
     }
 
     @Test
@@ -101,23 +104,21 @@ class BookServiceTest {
                 "Khoahoc", "quan su",
                 LocalDate.of(2010,05,12),
                 LocalDate.of(2015,05,15));
-        Book book2 = new Book(2,"Cac vi sao","PhamSang",
-                "Khoahoc",
-                "Sach kham pha",
-                LocalDate.of(2010, Month.APRIL,12),
-                LocalDate.of(2015, Month.AUGUST, 11));
-        bookRepository.saveAll(List.of(book,book2));
 
-        Book bookAfterUpdate =  new Book(1, "The War 2", "pham chi sang",
+        Book bookAfterUpdate =  new Book(1, "The war 2", "pham chi sang",
                 "Khoahoc", "quan su",
                 LocalDate.of(2010,05,12),
                 LocalDate.of(2015,05,15));
-
     }
 
     @Test
     void getABookId() {
-
+        given(bookRepository.existsById(1)).willReturn(true);
+        when(bookUnderService.getABookId(1)).thenReturn(Optional.of(new Book(1, "The war", "phamsang",
+                "Khoahoc", "quan su",
+                LocalDate.of(2010, 05, 12),
+                LocalDate.of(2015, 05, 15))));
+        assertThat(bookRepository.findById(1)).isEqualTo(bookUnderService.getABookId(1));
     }
 
     @Test
